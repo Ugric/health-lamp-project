2964858b5e29af622948cf4788d5490c56df96d1
W̷̛͒i̶̓͗l̷͗̌ľ̶͘ <62452284+Ugric@users.noreply.github.com>
2022-04-04 00:43:16 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+pins.analogWritePin(AnalogPin.P2, 1023)
+/*
+let brightness = 0
+const delaymax = 20 
+const steps = 1 
+pins.digitalWritePin(DigitalPin.P2, 1) 
 basic.forever(function () {
-	
+ const delay = brightness * delaymax
+ console.log(delay)
+if (Math.ceil(delay) != delaymax) pins.digitalWritePin(DigitalPin.P2, 0)
+basic.pause(delaymax - delay)
+if (Math.floor(delay)!= 0) pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(delay)
+ })
+basic.forever(function () {
+brightness = pins.analogReadPin(AnalogPin.P0) / 1023
+ basic.pause(delaymax)
+ })
+*/
+basic.forever(function () {
+    pins.analogWritePin(AnalogPin.P2, 1023)
+    basic.pause(10000 - 250)
+    pins.analogWritePin(AnalogPin.P2, 1011.5)
+    basic.pause(250)
+    radio.sendNumber(randint(60, 120))
 })
+radio.setGroup(1)
